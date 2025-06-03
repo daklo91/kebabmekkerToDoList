9c0a3c7f38ecac77896503cea3c7260b35147994
daklo91 <coffeedog91@gmail.com>
2025-06-03 11:10:37 +0200

    Create TodoList

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,14 @@
+function TodoList() {
+
+    return (
+      <div>
+       <ul>
+        <li>Temporary List Item</li>
+       </ul>
+       <button className="bg-lime-400 px-3 py-2 rounded-2xl text-xl font-bold">Kunde Ferdig</button>
+      </div>
+    )
+  }
+  
+  export default TodoList
+  
